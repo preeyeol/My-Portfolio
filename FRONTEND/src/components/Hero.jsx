a2a76bbdcc5c323847acdd9b15e7a79227378129
Preeti Raskoti <preeyeol27@gmail.com>
2024-12-18 10:38:16 +0545

    feat:Hero

--- FRONTEND/src/components/Hero.jsx
+++ FRONTEND/src/components/Hero.jsx
@@ -4,10 +4,12 @@ import "../css/Hero.css";
 const Hero = () => (
   <section className="hero">
     <h2>Welcome!</h2>
+
     <p>
       I’m a Full Stack Developer with a passion for creating impactful
       solutions.
     </p>
+    <img src="/path/to/your/image.jpg" alt="Hero Image" />
     <a href="#projects" className="hero-button">
       View My Work
     </a>
